2549bdba0d2db63c297d8717fb27da34c249e9d5
aysegulcayir <49029525+aysegulcayir@users.noreply.github.com>
2024-05-24 11:06:29 +0200

    Update output_transformations.py

--- src/dq_suite/output_transformations.py
+++ src/dq_suite/output_transformations.py
@@ -2,15 +2,17 @@ import pandas as pd
 from pyspark.sql.functions import col
 
 # for df_dqValidatie
-def extract_dq_validatie_data(check_name, dq_result):
+def extract_dq_validatie_data(df_name, dq_result):
     """
-    Function takes a json with the GX output and a string check_name and returns a dataframe.
+    Function takes a json dq_rules,and a string df_name and returns dataframe.
     
-    :param dq_result: A dictionary containing the Data Quality results from GX
-    :type dq_result: dict
-    :param check_name: Name of the run for reference purposes
-    :type check_name: str
-    :return: A dataframe with the DQ results per rule
+    :param df_dq_validatie: A df containing the valid result
+    :type df: DataFrame
+    :param dq_rules: A JSON string containing the Data Quality rules to be evaluated
+    :type dq_rules: str
+    :param df_name: Name of the tables
+    :type df_name: str
+    :return: A table df with the valid result DQ results, parsed from the extract_dq_validatie_data output
     :rtype: df.
     """
 
@@ -24,7 +26,7 @@ def extract_dq_validatie_data(check_name, dq_result):
         aantal_valide_records = element_count - unexpected_count
         expectation_type = result["expectation_config"]["expectation_type"]
         attribute = result["expectation_config"]["kwargs"].get("column")
-        dq_regel_id = f"{check_name}_{expectation_type}_{attribute}"
+        dq_regel_id = f"{df_name}_{expectation_type}_{attribute}"
         output=result["success"]
         output_text = "success" if output else "failure"
         extracted_data.append({
@@ -34,46 +36,44 @@ def extract_dq_validatie_data(check_name, dq_result):
             "dqDatum": run_time,
             "output": output_text,
         })
-        
     # Create a DataFrame
     df_dq_validatie = pd.DataFrame(extracted_data)
     return df_dq_validatie
 
-def extract_dq_afwijking_data(check_name, dq_result, df, unique_identifier):
+
+def extract_dq_afwijking_data(df_name, dq_result, df, unique_identifier):
     """
-    Function takes a json with the GX output and a string check_name and returns dataframe.
-    
-    :param dq_result: A dictionary containing the Data Quality results from GX
-    :type dq_result: dict
-    :param check_name: Name of the run for reference purposes
-    :type check_name: str
-    :param df: A dataframe with the actual data
-    :type: df
-    :param unique_identifier: The column name of the id values of the actual data
-    :type: str
-    :return: A dataframe with all unexpected values and their id
-    :rtype: df.
+    Function takes a json dq_rules and a string df_name and returns a DataFrame.
+
+    :param df_dq_validatie: A DataFrame containing the invalid (deviated) result
+    :type df: DataFrame
+    :param df_name: Name of the tables
+    :type df_name: str
+    : param unique_identifier: int comes from dq_rules
+    :type unique_identifier: int
+    :rtype: DataFrame
     """
-    
     # Extracting information from the JSON
     run_time = dq_result["meta"]["run_id"].run_time  # Access run_time attribute
     # Extracted data for df
     extracted_data = []
+  
     # To store unique combinations of value and IDs
     unique_entries = set()  
 
     for result in dq_result["results"]:
         expectation_type = result["expectation_config"]["expectation_type"]
         attribute = result["expectation_config"]["kwargs"].get("column")
-        dq_regel_id = f"{check_name}_{expectation_type}_{attribute}"
+        dq_regel_id = f"{df_name}_{expectation_type}_{attribute}"
         afwijkende_attribuut_waarde = result["result"].get("partial_unexpected_list", [])
         for value in afwijkende_attribuut_waarde:
             if value == None: 
                 filtered_df = df.filter(col(attribute).isNull())
+                ids = filtered_df.select(unique_identifier).rdd.flatMap(lambda x: x).collect()
             else:
-                filtered_df = df.filter(col(attribute)==value)
-            
-            ids = filtered_df.select(unique_identifier).rdd.flatMap(lambda x: x).collect()
+                    filtered_df = df.filter(col(attribute)==value)
+                    ids = filtered_df.select(unique_identifier).rdd.flatMap(lambda x: x).collect()
+
             for id_value in ids:
                 entry = (id_value)
                 if entry not in unique_entries:  # Check for uniqueness before appending
@@ -88,3 +88,98 @@ def extract_dq_afwijking_data(check_name, dq_result, df, unique_identifier):
     # Create a DataFrame
     df_dq_afwijking = pd.DataFrame(extracted_data)
     return df_dq_afwijking
+
+def create_brontabel(dq_rules):
+    """
+    Function takes the table name and their unique identifier from the provided Data Quality rules 
+    to create a DataFrame containing this metadata.
+
+    :param name: str comes from dq_rules
+    :type name: str
+    :param unique_identifier: int comes from dq_rules
+    :type unique_identifier: int
+    :rtype: DataFrame
+    :return: df_brontable
+    :rtype: DataFrame
+    """
+    extracted_data = []
+    for param in dq_rules["tables"]:
+        name = param["table_name"]  
+        unique_identifier = param["unique_identifier"]
+        extracted_data.append({
+            "name": name,
+            "unique_identifier": unique_identifier
+        })
+    
+    df_brontable = pd.DataFrame(extracted_data)
+    return df_brontable
+
+def create_bronattribute(dq_rules):
+    """
+    This function takes attributes/columns for each table specified in the Data Quality rules and creates a DataFrame containing these attribute details.
+
+    :param dq_rules: Data Quality rules
+    :type dq_rules: dict
+    :return: df_bronattribuut
+    :rtype: DataFrame
+    """
+    extracted_data = []
+    used_ids = set()  # To keep track of used IDs
+    for param in dq_rules["tables"]:
+        BronTabel = param["table_name"] 
+        for rule in param["rules"]:
+            parameters = rule.get("parameters", [])
+            for parameter in parameters:
+                if isinstance(parameter, dict) and "column" in parameter:
+                    attribute_name = parameter["column"]
+                    # Create a unique ID
+                    unique_id = f"{BronTabel}_{attribute_name}"
+                    # Check if the ID is already used
+                    if unique_id not in used_ids:
+                        used_ids.add(unique_id)
+                        extracted_data.append({
+                            "name": attribute_name,
+                            "BronTabel": BronTabel,
+                            "id": unique_id
+                        })
+    
+    df_bronattribuut = pd.DataFrame(extracted_data)
+    return df_bronattribuut
+
+
+def create_dqRegel(dq_rules):
+    """
+    Function extracts information about Data Quality rules applied to each attribute/column for tables specified in the Data Quality rules and creates a DataFrame containing these rule details.
+
+    :param BronTabel: str comes from dq_rules
+    :type BronTabel: str
+    :param rule_name: str comes from dq_rules
+    :type rule_name: str
+    :param attribute_name: str comes from dq_rules
+    :type attribute_name: str
+    :return: df_dqRegel
+    :rtype: DataFrame
+    """
+    extracted_data = []
+    for param in dq_rules["tables"]:
+        BronTabel = param["table_name"] 
+        for rule in param["rules"]:
+            rule_name = rule["rule_name"]
+            parameters = rule.get("parameters", [])
+            for parameter in parameters:
+                if isinstance(parameter, dict) and "column" in parameter:
+                    attribute_name = parameter["column"]
+                    extracted_data.append({
+                        "id": f"{BronTabel}_{rule_name}_{attribute_name}",
+                        "bronAttibuteId": f"{BronTabel}_{attribute_name}" ,
+                        "BronTabel name": BronTabel
+                    })
+    
+    df_dqRegel = pd.DataFrame(extracted_data)
+    return df_dqRegel
+
+def convert_to_string(df):
+    # Convert each column to string
+    for col in df.columns:
+        df = df.withColumn(col, df[col].astype("string"))
+    return df
