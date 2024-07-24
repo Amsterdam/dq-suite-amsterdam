80020f891dbda9b16177088ca2f2b37be3ff4b95
ArthurKordes <75675106+ArthurKordes@users.noreply.github.com>
2024-07-24 15:20:33 +0200

    Update __init__.py

--- src/dq_suite/__init__.py
+++ src/dq_suite/__init__.py
@@ -1,7 +1,13 @@
 """DQ API."""
 
-from dq_suite.input_validator import validate_dqrules
 from dq_suite.df_checker import df_check
+from dq_suite.input_helpers import (
+    validate_dqrules,
+    expand_input,
+    export_schema,
+    fetch_schema_from_github,
+    generate_dq_rules_from_schema
+)
 from dq_suite.output_transformations import (
     extract_dq_validatie_data,
     extract_dq_afwijking_data
@@ -10,5 +16,6 @@ from dq_suite.output_transformations import (
 # Use __all__ to let developers know what is part of the public API.
 __all__ = [
     "validate_dqrules",
-    "df_check"
+    "df_check",
+    "export_schema"
 ]
