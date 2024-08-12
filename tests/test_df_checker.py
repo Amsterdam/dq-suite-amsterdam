cdb0b9c5cde8ecb3ccdcdc17f55654560a675434
bas <b.schotten@amsterdam.nl>
2024-08-12 10:09:23 +0200

    Add first unit test with test data

---
+++ tests/test_df_checker.py
@@ -0,0 +1,20 @@
+import pytest
+
+from src.dq_suite.df_checker import read_data_quality_rules_from_json
+
+
+class TestReadDataQualityRulesFromJson:
+    dummy_file_path = "nonexistent_file_path"
+    real_file_path = "test_data/dq_rules.json"
+
+    def test_read_data_quality_rules_from_json_raises_file_not_found_error(
+            self):
+        with pytest.raises(FileNotFoundError):
+            read_data_quality_rules_from_json(file_path=self.dummy_file_path)
+
+    def test_read_data_quality_rules_from_json_returns_json_string(
+            self):
+        dq_rules_json_string = read_data_quality_rules_from_json(
+            file_path=self.real_file_path)
+        assert type(dq_rules_json_string) is str
+        assert "tables" in dq_rules_json_string
