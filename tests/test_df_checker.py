bdc9b98fbc4a7530cf45b8123da3de7687384094
bas <b.schotten@amsterdam.nl>
2024-08-12 16:00:08 +0200

    Add TestValidationSettings

--- tests/test_df_checker.py
+++ tests/test_df_checker.py
@@ -1,6 +1,28 @@
 import pytest
 
-from src.dq_suite.df_checker import read_data_quality_rules_from_json
+from src.dq_suite.df_checker import read_data_quality_rules_from_json, ValidationSettings
+
+
+class TestValidationSettings:
+    validation_settings_obj = ValidationSettings(
+        spark_session="test",  # TODO: fix local spark
+        catalog_name="the_catalog",
+        table_name="the_table",
+        check_name="the_check",
+    )
+
+    def test_initialise_or_update_attributes_updates_as_expected(self):
+        expected_suite_name = "the_check_expectation_suite"
+
+        assert self.validation_settings_obj.expectation_suite_name is None
+        assert self.validation_settings_obj.checkpoint_name is None
+        assert self.validation_settings_obj.run_name is None
+        self.validation_settings_obj.initialise_or_update_attributes()
+        assert (self.validation_settings_obj.expectation_suite_name ==
+                expected_suite_name)
+        assert (expected_suite_name in
+                self.validation_settings_obj.data_context
+                .list_expectation_suites()[0].__getstate__().values())
 
 
 class TestReadDataQualityRulesFromJson:
