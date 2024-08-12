93161d03e19b1c5cc2c4272dd50ca847eca61c66
bas <b.schotten@amsterdam.nl>
2024-08-12 10:06:15 +0200

    Fix __init__.py

--- src/dq_suite/__init__.py
+++ src/dq_suite/__init__.py
@@ -1,7 +1,7 @@
 """DQ API."""
 
-from dq_suite.df_checker import df_check
-from dq_suite.input_helpers import export_schema, validate_dqrules
+from src.dq_suite.common import export_schema, validate_and_load_dqrules
+from src.dq_suite.df_checker import validate_dataframes
 
 # Use __all__ to let developers know what is part of the public API.
-__all__ = ["validate_dqrules", "df_check", "export_schema"]
+__all__ = ["validate_and_load_dqrules", "validate_dataframes", "export_schema"]
