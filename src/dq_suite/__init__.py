579095f56ea6bca4e38bd8360f05f6b81749b11e
bas <b.schotten@amsterdam.nl>
2024-08-15 14:05:17 +0200

    Fix __init__.py

--- src/dq_suite/__init__.py
+++ src/dq_suite/__init__.py
@@ -1,8 +1,8 @@
 """DQ API."""
 
-from src.dq_suite.common import ValidationSettings
-from src.dq_suite.df_checker import run
-from src.dq_suite.input_helpers import schema_to_json_string
+from .common import ValidationSettings
+from .df_checker import run
+from .input_helpers import schema_to_json_string
 
 # Use __all__ to let developers know what is part of the public API.
 __all__ = ["schema_to_json_string", "run", "ValidationSettings"]
