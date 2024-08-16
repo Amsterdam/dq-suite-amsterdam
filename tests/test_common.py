317beb8c38bb2c229f48a1136ba2968390f6eadb
bas <b.schotten@amsterdam.nl>
2024-08-16 19:38:18 +0200

    Fix broken tests in test_common.py

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,28 +1,30 @@
-from typing import Any
+from dataclasses import is_dataclass
+from unittest.mock import Mock
 
 import pytest
+from pyspark.sql import SparkSession
 
 from src.dq_suite.common import (
     Rule,
     RulesDict,
-    RulesList,
     ValidationSettings,
     get_full_table_name,
 )
 
 
 class TestRule:
-    rule_obj = Rule(rule_name="the_rule", parameters=[{"q": 42}])
+    expected_rule_name = "the_rule"
+    expected_parameters = [{"q": 42}]
+    rule_obj = Rule(
+        rule_name=expected_rule_name, parameters=expected_parameters
+    )
+
+    def test_rule_is_dataclass(self):
+        assert is_dataclass(self.rule_obj)
 
     def test_get_value_from_rule_by_existing_key(self):
-        rule_name = self.rule_obj["rule_name"]
-        parameters = self.rule_obj["parameters"]
-
-        assert rule_name == "the_rule"
-        assert type(rule_name) is str
-
-        assert parameters == [{"q": 42}]
-        assert type(parameters) is list[dict[str, Any]]  # TODO: fix
+        assert self.rule_obj["rule_name"] == self.expected_rule_name
+        assert self.rule_obj["parameters"] == self.expected_parameters
 
     def test_get_value_from_rule_by_non_existing_key(self):
         with pytest.raises(KeyError):
@@ -31,24 +33,25 @@ class TestRule:
 
 class TestRulesDict:
     rule_obj = Rule(rule_name="the_rule", parameters=[{"q": 42}])
+    expected_unique_identifier = "id"
+    expected_table_name = "the_table"
+    expected_rules_list = [rule_obj]
     rules_dict_obj = RulesDict(
-        unique_identifier="id", table_name="the_table", rules_list=[rule_obj]
+        unique_identifier=expected_unique_identifier,
+        table_name=expected_table_name,
+        rules_list=expected_rules_list,
     )
 
+    def test_rules_dict_is_dataclass(self):
+        assert is_dataclass(self.rules_dict_obj)
+
     def test_get_value_from_rule_dict_by_existing_key(self):
-        unique_identifier = self.rules_dict_obj["unique_identifier"]
-        table_name = self.rules_dict_obj["table_name"]
-        rules_list = self.rules_dict_obj["rules_list"]
-
-        assert unique_identifier == "id"
-        assert type(unique_identifier) is str
-
-        assert table_name == "the_table"
-        assert type(table_name) is str
-
-        assert rules_list == [self.rule_obj]
-        assert type(rules_list) is RulesList  # TODO: fix
-        assert type(rules_list[0]) is Rule
+        assert (
+            self.rules_dict_obj["unique_identifier"]
+            == self.expected_unique_identifier
+        )
+        assert self.rules_dict_obj["table_name"] == self.expected_table_name
+        assert self.rules_dict_obj["rules_list"] == self.expected_rules_list
 
     def test_get_value_from_rule_dict_by_non_existing_key(self):
         with pytest.raises(KeyError):
@@ -67,8 +70,9 @@ def test_get_full_table_name():
 
 
 class TestValidationSettings:
+    spark_session_mock = Mock(spec=SparkSession)
     validation_settings_obj = ValidationSettings(
-        spark_session="test",  # TODO: fix local spark
+        spark_session=spark_session_mock,
         catalog_name="the_catalog",
         table_name="the_table",
         check_name="the_check",
