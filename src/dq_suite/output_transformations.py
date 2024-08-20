e34694cd7fa64fd110f6e9ea2c9f898a12b2ad23
bas <b.schotten@amsterdam.nl>
2024-08-20 12:38:34 +0200

    Fix schema imports

--- src/dq_suite/output_transformations.py
+++ src/dq_suite/output_transformations.py
@@ -3,11 +3,11 @@ from typing import Any, List
 from pyspark.sql import DataFrame, Row, SparkSession
 from pyspark.sql.functions import col
 
-from src.schemas.afwijking import SCHEMA as AFWIJKING_SCHEMA
-from src.schemas.bronattribuut import SCHEMA as BRONATTRIBUUT_SCHEMA
-from src.schemas.brontabel import SCHEMA as BRONTABEL_SCHEMA
-from src.schemas.regel import SCHEMA as REGEL_SCHEMA
-from src.schemas.validatie import SCHEMA as VALIDATIE_SCHEMA
+from ..schemas.afwijking import SCHEMA as AFWIJKING_SCHEMA
+from ..schemas.bronattribuut import SCHEMA as BRONATTRIBUUT_SCHEMA
+from ..schemas.brontabel import SCHEMA as BRONTABEL_SCHEMA
+from ..schemas.regel import SCHEMA as REGEL_SCHEMA
+from ..schemas.validatie import SCHEMA as VALIDATIE_SCHEMA
 
 from .common import (
     DataQualityRulesDict,
