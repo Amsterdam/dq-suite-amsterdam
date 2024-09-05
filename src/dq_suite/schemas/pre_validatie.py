886760c452e969512268d36e37f1557728486eaf
ArthurKordes <ArthurKordes>
2024-09-05 14:56:07 +0000

    Adds the new regel ID and allows for merging dfs instead of only append
    I will add more columns to the output tables, but wanted to share this commit in order to not bloat the PR too much

--- src/dq_suite/schemas/pre_validatie.py
+++ src/dq_suite/schemas/pre_validatie.py
@@ -2,6 +2,10 @@ from pyspark.sql.types import StructType
 
 SCHEMA = (
     StructType()
+    .add("aantalValideRecords", "long")
+    .add("aantalReferentieRecords", "long")
+    .add("dqDatum", "timestamp")
+    .add("dqResultaat", "string")
     .add("regelNaam", "string")
     .add("regelParameters", "string")
     .add("bronTabelId", "string")
