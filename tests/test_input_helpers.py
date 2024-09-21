650839d415730e5782c3fc1355237c5446798be3
bas <b.schotten@amsterdam.nl>
2024-09-21 16:11:27 +0200

    Add TestValidateDataSet

--- tests/test_input_helpers.py
+++ tests/test_input_helpers.py
@@ -4,7 +4,7 @@ from tests import TEST_DATA_FOLDER
 from src.dq_suite.input_helpers import (
     load_data_quality_rules_from_json_string,
     read_data_quality_rules_from_json,
-    validate_data_quality_rules_dict,
+    validate_data_quality_rules_dict, validate_dataset,
 )
 
 
@@ -54,9 +54,77 @@ class TestValidateDataQualityRulesDict:
         self,
     ):
         with pytest.raises(TypeError):
-            validate_data_quality_rules_dict(data_quality_rules_dict="bla")
+            validate_data_quality_rules_dict(
+                data_quality_rules_dict="wrong_type")
 
     def test_validate_data_quality_rules_dict(self):
         validate_data_quality_rules_dict(
             data_quality_rules_dict=self.data_quality_rules_dict
         )
+
+
+class TestValidateDataSet:
+    real_file_path = f"{TEST_DATA_FOLDER}/dq_rules.json"
+    data_quality_rules_json_string = read_data_quality_rules_from_json(
+        file_path=real_file_path
+    )
+    data_quality_rules_dict = load_data_quality_rules_from_json_string(
+        dq_rules_json_string=data_quality_rules_json_string
+    )
+    def test_validate_dataset_without_dataset_key_raises_key_error(
+        self,
+    ):
+        with pytest.raises(KeyError):
+            validate_dataset(data_quality_rules_dict=
+                             {"some_other_thing": "with_some_value"})
+
+    def test_validate_dataset_without_dict_typed_value_raises_type_error(
+        self,
+    ):
+        with pytest.raises(TypeError):
+            validate_dataset(data_quality_rules_dict=
+                             {"dataset": "with_some_value"})
+
+    def test_validate_dataset_without_name_key_raises_key_error(
+        self,
+    ):
+        with pytest.raises(KeyError):
+            validate_dataset(data_quality_rules_dict=
+                             {"dataset": dict()})
+
+    def test_validate_dataset_without_layer_key_raises_key_error(
+        self,
+    ):
+        with pytest.raises(KeyError):
+            validate_dataset(data_quality_rules_dict=
+                             {"dataset": {
+                                 "name": 123
+                             }
+                              })
+
+    def test_validate_dataset_without_str_typed_name_raises_type_error(
+        self,
+    ):
+        with pytest.raises(TypeError):
+            validate_dataset(data_quality_rules_dict=
+                             {"dataset": {
+                                 "name": 123,
+                                 "layer": 456
+                             }
+                              })
+
+    def test_validate_dataset_without_str_typed_layer_raises_type_error(
+        self,
+    ):
+        with pytest.raises(TypeError):
+            validate_dataset(data_quality_rules_dict=
+                             {"dataset": {
+                                 "name": "the_dataset_name",
+                                 "layer": 456
+                             }
+                              })
+
+    def test_validate_dataset_works_as_expected(
+        self,
+    ):
+        validate_dataset(data_quality_rules_dict=self.data_quality_rules_dict)
