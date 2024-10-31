b6f67c4851c094470f2b1c8143866bbf59c34ead
esra ozturk <esra.ozturk@amsterdam.nl>
2024-10-31 10:37:57 +0100

    Added unit tests and test schema

--- tests/test_data/test_schema.py
+++ tests/test_data/test_schema.py
@@ -3,3 +3,10 @@ from pyspark.sql.types import StructType
 SCHEMA = (
     StructType().add("the_string", "string").add("the_timestamp", "timestamp")
 )
+
+SCHEMA2 = (
+    StructType()
+    .add("voornaam", "string")
+    .add("achternaam", "string")
+    .add("leeftijd", "integer")
+)
