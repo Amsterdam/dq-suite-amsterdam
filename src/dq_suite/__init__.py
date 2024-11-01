fa8c3bceb41b00dd7099777575644991b55022d5
bas <b.schotten@amsterdam.nl>
2024-11-01 11:37:37 +0100

    Delete duplicate schema_to_json_string and export_schema functions, move ad-hoc functions to other.py

--- src/dq_suite/__init__.py
+++ src/dq_suite/__init__.py
@@ -2,7 +2,7 @@
 
 from .common import ValidationSettings
 from .df_checker import run
-from .validation_input import schema_to_json_string
+from .other import export_schema_to_json_string
 
 # Use __all__ to let developers know what is part of the public API.
-__all__ = ["schema_to_json_string", "run", "ValidationSettings"]
+__all__ = ["export_schema_to_json_string", "run", "ValidationSettings"]
