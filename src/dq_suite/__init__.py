5a353e17a25c301ff56d8a39f40e31caf788f293
Bas <SSchotten@users.noreply.github.com>
2024-11-18 14:34:54 +0100

    Fix - import readme (#72)
    * Fix info in README.md about import
    
    * Change check_name to validation_name
    
    * Version to 0.11.4
    
    * Changed run to run_validation
    
    ---------
    
    Co-authored-by: bas <b.schotten@amsterdam.nl>
    Co-authored-by: ArthurKordes <ArthurKordes>

--- src/dq_suite/__init__.py
+++ src/dq_suite/__init__.py
@@ -2,7 +2,7 @@
 
 # from .common import ValidationSettings
 # from .other import schema_to_json_string
-# from .validation import run
+# from .validation import run_validation
 #
 # # Use __all__ to let developers know what is part of the public API.
-# __all__ = ["schema_to_json_string", "run", "ValidationSettings"]
+# __all__ = ["schema_to_json_string", "run_validation", "ValidationSettings"]
