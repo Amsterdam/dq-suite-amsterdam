2b962000e1d5bef5ccdcebb625ff3880c50e4c8e
bas <b.schotten@amsterdam.nl>
2025-03-10 16:51:29 +0100

    Move custom Slack output to _get_validation_text_blocks method

--- src/dq_suite/custom_renderers/slack_renderer.py
+++ src/dq_suite/custom_renderers/slack_renderer.py
@@ -4,6 +4,11 @@ from great_expectations.checkpoint.checkpoint import CheckpointResult
 
 from typing import Literal
 
+from great_expectations.core import ExpectationSuiteValidationResult
+from great_expectations.data_context.types.resource_identifiers import \
+    ValidationResultIdentifier
+
+
 # https://medium.com/@jojo-data/how-to-create-a-custom-module-in-great-expectations-efd0a6ed704a
 
 
@@ -19,14 +24,54 @@ class CustomSlackNotificationAction(SlackNotificationAction):
                 and not success
         )
 
-    # @override
+    def _get_validation_text_blocks(self, validation_result_identifier:
+    ValidationResultIdentifier, suite_validation_result:
+    ExpectationSuiteValidationResult, action_context: ActionContext) -> list[dict]:
+
+        validation_text_blocks = self._render_validation_result(
+            result_identifier=validation_result_identifier,
+            result=suite_validation_result,
+            action_context=action_context,
+        )
+
+        # Add sample of unexpected values
+        summary_text = validation_text_blocks[0]["text"]["text"]
+        summary_text += "\n-----------------------\n"
+        if not suite_validation_result.success:
+            for result in suite_validation_result.results:
+                if not result.success:
+                    expectation_info = result["expectation_config"]["meta"]
+                    summary_text += (
+                        f"""
+                        \n *Column*:
+                         {expectation_info['column_name']} / 
+                        *Expectation*:
+                         {expectation_info['expectation_name']}\n
+                        """
+                    )
+
+                    results = result.result
+                    summary_text += (
+                        f"""
+                        \n *Sample unexpected values*: 
+                        {results['partial_unexpected_list'][:3]}
+                         / *Unexpected / total count*: 
+                        {results['unexpected_count']} / {results['element_count']}\n
+                        """
+                    )
+
+                    summary_text += "-----------------------\n"
+
+        validation_text_blocks[0]["text"]["text"] = summary_text
+
+        return validation_text_blocks
+
     def run(
         self,
         checkpoint_result: CheckpointResult,
         action_context: ActionContext | None = None,
     ) -> dict:
         success = checkpoint_result.success or False
-        checkpoint_name = checkpoint_result.checkpoint_config.name
         result = {"slack_notification_result": "none required"}
 
         if not self._should_notify(success=success, notify_on=self.notify_on):
@@ -34,52 +79,21 @@ class CustomSlackNotificationAction(SlackNotificationAction):
 
         checkpoint_text_blocks: list[dict] = []
         for (
-            validation_result_suite_identifier,
-            validation_result_suite,
+            validation_result_identifier,
+            suite_validation_result,
         ) in checkpoint_result.run_results.items():
-            validation_text_blocks = self._render_validation_result(
-                result_identifier=validation_result_suite_identifier,
-                result=validation_result_suite,
-                action_context=action_context,
+            validation_text_blocks = self._get_validation_text_blocks(
+                validation_result_identifier=validation_result_identifier,
+                suite_validation_result=suite_validation_result,
+                action_context=action_context
             )
-
-            # Add sample of unexpected values
-            summary_text = validation_text_blocks[0]["text"]["text"]
-            summary_text += "\n-----------------------\n"
-            if not validation_result_suite.success:
-                for result in validation_result_suite.results:
-                    if not result.success:
-                        expectation_info = result["expectation_config"]["meta"]
-                        summary_text += (
-                            f"\n *Table*:"
-                            f" {expectation_info['table_name']} / "
-                            f"*Column*:"
-                            f" {expectation_info['column_name']} /  "
-                            f"*Expectation*:"
-                            f" {expectation_info['expectation_name']}\n"
-                        )
-
-                        results = result.result
-                        summary_text += (
-                            f"\n *Sample unexpected values*: "
-                            f"{results['partial_unexpected_list'][:3]}"
-                            f" / *Unexpected percentage*: "
-                            f""
-                            f""
-                            f"{results['unexpected_percent_total']}\n"
-                        )
-
-                        summary_text += "-----------------------\n"
-
-            validation_text_blocks[0]["text"]["text"] = summary_text
-
             checkpoint_text_blocks.extend(validation_text_blocks)
 
         payload = self.renderer.concatenate_text_blocks(
             action_name=self.name,
             text_blocks=checkpoint_text_blocks,
             success=success,
-            checkpoint_name=checkpoint_name,
+            checkpoint_name=checkpoint_result.checkpoint_config.name,
             run_id=checkpoint_result.run_id,
         )
 
